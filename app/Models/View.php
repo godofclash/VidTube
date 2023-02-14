31f8742f0e24ab5e8b653dc52912252caf42ed79
= <=>
2023-02-14 23:14:22 +0100

    solved error with views

--- app/Models/View.php
+++ app/Models/View.php
@@ -4,19 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Events\ChildCreated;
 
 class View extends Model
 {
     use HasFactory;
-
-    protected static function boot() {
-        parent::boot();
-
-        static::created(function ($child) {
-            event(new ChildCreated($child));
-        });
-    }   
 
     protected $fillable = [
         'user_id', 
